5f19940cdfac905cb4f0bf47c9a927a94df20e9b
suzvasilii <vevjs@mail.ru>
2026-04-04 02:14:26 +0300

    done

--- backend/app/routes/cart.py
+++ backend/app/routes/cart.py
@@ -31,7 +31,7 @@ def add_to_cart(request: AddToCartRequest, db: Session = Depends(get_db)):
     updated_cart = service.add_to_cart(request.cart, item)
     return {"cart": updated_cart}
 
-@router.get("", response_model=CartResponse, status_code=status.HTTP_200_OK)
+@router.post("", response_model=CartResponse, status_code=status.HTTP_200_OK)
 def get_cart(cart_data: Dict[int, int], db: Session = Depends(get_db)):
     service = CartService(db)
     return service.get_cart_details(cart_data)
